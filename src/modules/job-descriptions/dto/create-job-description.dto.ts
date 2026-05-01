5d64672cd73b9e6db36eaa1ef70357384aa1cd2d
Đặng Hữu Long <danghuulong394@gmail.com>
2026-05-01 16:24:50 +0700

    feat: implement job description create dto

---
+++ src/modules/job-descriptions/dto/create-job-description.dto.ts
@@ -0,0 +1,45 @@
+import { IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class CreateJobDescriptionDto {
+  @IsString()
+  @MaxLength(200)
+  title!: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(200)
+  companyName?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(150)
+  department?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
+  location?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  employmentType?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  seniority?: string;
+
+  @IsString()
+  rawText!: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  parserVersion?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  createdById?: string;
+}
