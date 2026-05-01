25ecac57f3360bb65f5e7805c5ef7edfafff0bbb
Đặng Hữu Long <danghuulong394@gmail.com>
2026-05-01 21:37:06 +0700

    feat: wire ai module into job descriptions

--- src/modules/job-descriptions/job-descriptions.module.ts
+++ src/modules/job-descriptions/job-descriptions.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { JobDescriptionsController } from './job-descriptions.controller';
 import { JobDescriptionsService } from './job-descriptions.service';
+import { AiModule } from '../../integrations/ai/ai.module';
 
 @Module({
+  imports: [AiModule],
   controllers: [JobDescriptionsController],
   providers: [JobDescriptionsService],
   exports: [JobDescriptionsService],
