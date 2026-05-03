d7a5831d36c08c62191b0b1eb13bf688420f521c
Đặng Hữu Long <danghuulong394@gmail.com>
2026-05-03 22:34:15 +0700

    feat(evaluations): export evaluation status enum

--- src/common/enums/index.ts
+++ src/common/enums/index.ts
@@ -2,3 +2,4 @@ export * from './parse-status.enum';
 export * from './file-asset-status.enum';
 export * from './resume-file-type.enum';
 export * from './application-status.enum';
+export * from './evaluation-status.enum';
