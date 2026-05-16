975eaa91c064fd3561b60182bdaa63d9f7192d2b
Đặng Hữu Long <danghuulong394@gmail.com>
2026-05-16 15:57:43 +0700

    feat(auth): add auth user primitives

---
+++ src/common/decorators/current-user.decorator.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+import { AuthUser } from '../types/auth-user.type';
+
+type RequestWithUser = {
+  user?: AuthUser;
+};
+
+export const CurrentUser = createParamDecorator(
+  (_data: unknown, ctx: ExecutionContext): AuthUser | undefined => {
+    const request = ctx.switchToHttp().getRequest<RequestWithUser>();
+    return request.user;
+  },
+);
